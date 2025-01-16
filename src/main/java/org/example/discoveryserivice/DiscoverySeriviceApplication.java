990b4f89a831b158297314ae00d50f2e1a9119d0
mayur pardeshi <pardeshimayur0@gmail.com>
2025-01-16 16:28:58 +0530

    initial commit

---
+++ src/main/java/org/example/discoveryserivice/DiscoverySeriviceApplication.java
@@ -0,0 +1,13 @@
+package org.example.discoveryserivice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoverySeriviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoverySeriviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/discoveryserivice/DiscoverySeriviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.discoveryserivice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoverySeriviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
